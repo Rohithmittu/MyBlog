d005c932d49dc9902751a3d3f15bbafe9d9ea92a
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-03-12 00:41:32 +0530

    understood the way how the auth has been got through store and surrounded all router with store

--- MyBlog/src/components/AuthLayout.jsx
+++ MyBlog/src/components/AuthLayout.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
-import {  useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function Protected({ children, authentication = true }) {      //  --> authentication has not done yet its just passed for future references
 
 
-function Protected({children,authentication = true}) {
 
-  const authStatus  = useSelector((state) => state.auth.status)
+  const authStatus = useSelector((state) => state.auth.status); // this state has been got from auth through 
+                                                                // redux store and it is the syntax for useSlector from redux
+                                                                //  store (refer docs)
 
-  const navigate = useNavigate()
+                                                                // auth comes from authslice which is name : "auth" 
+                                                                // through store which is placed as a provider in main.jsx
+                                                                // main -> store -> authslice -> authlayout (auth is passed) 
+
+  const navigate = useNavigate();
 
   const [loader, setLoader] = useState(true);
 
   useEffect(() => {
-
-    if(authentication && authStatus !== authentication){
-      navigate("/login")
-
-    }else if(!authentication && authStatus !== authentication){
-      navigate('/');
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
     }
 
     setLoader(false);
-
-
-    
-  }, [authStatus,authentication,navigate])
-  
-
+  }, [authStatus, authentication, navigate]);
 
   return loader ? null : <>{children}</>;
-
 }
 
-export default Protected
+export default Protected;
