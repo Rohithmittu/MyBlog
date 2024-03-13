cae668c0ba440be1e95b9f4e978cc862967ea85c
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-03-13 14:46:34 +0530

    completed the editpost only for the admin,and viewing  posts in allpost..Implemented the login and sign up pages through index.js file to make ot easy importing

--- MyBlog/src/pages/Home.jsx
+++ MyBlog/src/pages/Home.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import appwriteService from "../appwrite/config"
+
+import Container from '../components/container/Container'
+
+import PostCard from "../components/PostCard"
+
 
 function Home() {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+
+    appwriteService.getPosts([]).then((posts)=>{
+      if(posts){
+        setPosts(posts.documents)
+      }
+    })
+    
+  }, [])
+
+
+  if(posts.length === 0){
+    return (
+      <div className='w-full py-8'>
+      <Container>
+        <div className='flex flex-wrap'>
+          <h1>Login to read the posts</h1>
+        </div>
+      </Container>
+    </div>
+    )
+  }
+  
+
   return (
-    <div>Home</div>
+    <div className='w-full py-8'>
+      <Container>
+        <div className='flex flex-wrap'>
+          {posts.map((post) => (
+            <div className='p-2 w-1/4' key={post.$id}>
+              <PostCard  {...post} />  // because we passes anobject in postcard so we pass it by destructuring
+            </div>
+
+          ))}
+        </div>
+      </Container>
+    </div>
   )
 }
 
